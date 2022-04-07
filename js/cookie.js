6f1c6a759248d07ff11fefbf03adc7f5ce54ff24
Thet-Rain <92264416+Thet-Rain@users.noreply.github.com>
2022-04-07 02:09:05 -0400

    Shopping Cart now save on cookie

---
+++ js/cookie.js
@@ -0,0 +1,42 @@
+
+
+//readCookie on load;
+if (document.cookie != 0)
+{
+  var cookieArray = readCookie();
+  loadCart(cookieArray);
+
+}
+
+//Write Cookie
+
+$(document).on("click", ".shopping-cart" ,function () {
+
+var index = this.value;
+
+if (document.cookie == 0)
+{
+var cookieString = "index0=" + index + ";";
+document.cookie = escape(cookieString);
+}
+else
+{
+  var length = readCookie().length;
+  var cookieString = "index" + length + "=" + index + ";";
+  document.cookie += escape(cookieString);
+}
+loadCart(readCookie());
+});
+
+
+//read cookie
+function readCookie()
+{
+  var cookieArray = unescape(document.cookie).split(';');
+  var cookieArray2 = [];
+  for (var i = 0; i < cookieArray.length -1 ; i++)
+  {
+    cookieArray2.push(cookieArray[i].split('='));
+  }
+  return cookieArray2;
+}
